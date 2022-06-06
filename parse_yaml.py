dda507a6d79052cb33cfe69f5d3ef1964997a392
Ralf Mikk <ralmik@ttu.ee>
2022-06-06 14:17:47 +0300

    eksam Parsing yaml

--- parse_yaml.py
+++ parse_yaml.py
@@ -1,9 +1,14 @@
 import yaml, requests
 
-url = 'http://upload.itcollege.ee/~aleksei/eksam.yaml'
 
 def parse_yaml(url):
-    
-    # your code here
-    
-    return name_of_the_user
+    yaml_content = requests.get(url)
+    content_list = yaml.safe_load(yaml_content.text)
+    for dicts in content_list:
+        for key, value in dicts.items():
+            if dicts[key]["permission"] == "admin":
+                return dicts[key]["name"]
+
+
+if __name__ == '__main__':
+    print(parse_yaml('http://upload.itcollege.ee/~aleksei/eksam.yaml'))
